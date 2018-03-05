f4c5ebc02cabbf59129dfae1a2c6758faaca6c0f
Charlebois Christophe <Panathes@users.noreply.github.com>
2018-03-05 15:23:45 +0100

    Start and Restart button setup

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ var start = document.getElementById('game-start');
 var gameover = document.getElementById('game-over');
 var game = document.getElementById('game');
 var button = document.querySelector('button');
+var startButton = document.querySelector('#game-start > button');
+var restartButton = document.querySelector('#game-over > button');
 
 var time = document.querySelector('span');
 
 // Start game
 
-button.addEventListener('click', function() {
+startButton.addEventListener('click', function() {
   time.textContent = 20;
   start.classList.remove('is-open');
   game.classList.add('is-open');
-timer();
 });
 
+//Restart game
+restartButton.addEventListener('click', function(){
+  time.textContent = 20;
+  gameover.classList.remove('is-open');
+  game.classList.add('is-open');
+});
+
+timer();
 // Countdown
 
 function timer() {
@@ -21,7 +30,7 @@ function timer() {
     time.textContent --
   }
   if (time.textContent === '0') {
-    gameover.classList.remove('is-open');
+
     gameover.classList.add('is-open');
   };
   setTimeout(timer, 1000);
