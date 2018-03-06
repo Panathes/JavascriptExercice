8288a436a0f06e4d0b95b97bb36f7e982ead4387
Charlebois Christophe <Panathes@users.noreply.github.com>
2018-03-06 14:00:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ var game = document.getElementById('game');
 var button = document.querySelector('button');
 var startButton = document.querySelector('#game-start > button');
 var restartButton = document.querySelector('#game-over > button');
+var originalFlag = document.querySelector('.flag');
+
+var title = document.querySelector('h2');
 
 var time = document.querySelector('span');
+
+// random name
+var randomTitle = flags[Math.floor(Math.random() * flags.length)].name;
+
+//randomCards
+function randomCards() {
+for (var i = 0; i < array.length; i++) {
+  
+}
+}
 
 // Start game
 
@@ -13,6 +26,7 @@ startButton.addEventListener('click', function() {
   time.textContent = 20;
   start.classList.remove('is-open');
   game.classList.add('is-open');
+  title.textContent = randomTitle;
 });
 
 //Restart game
@@ -30,8 +44,7 @@ function timer() {
     time.textContent --
   }
   if (time.textContent === '0') {
-
-    gameover.classList.add('is-open');
+  gameover.classList.add('is-open');
   };
   setTimeout(timer, 1000);
 };
