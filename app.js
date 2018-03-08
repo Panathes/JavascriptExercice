5ce6beddbc43e9f71efb1ae799c1520327eb1a68
Charlebois Christophe <Panathes@users.noreply.github.com>
2018-03-08 14:03:59 +0100

    Update app.js function randomCards

--- app.js
+++ app.js
@@ -5,29 +5,43 @@ var button = document.querySelector('button');
 var startButton = document.querySelector('#game-start > button');
 var restartButton = document.querySelector('#game-over > button');
 
+var rand = function(max) {
+  return Math.floor(Math.random() * max);
+}
 
 var title = document.querySelector('h2');
 
 var time = document.querySelector('span');
 
 // random name
-var randomTitle = flags[Math.floor(Math.random() * flags.length)].name;
+ var  randomTitle = flags[Math.floor(Math.random() * flags.length)].name;
+//var goodIndex = flags[rand(flag.length)];
 
+//var randomTitle = flags[rand(flag.length)].name;
+
+
+var random = rand();
 // randomflags
 var randomflags = flags[Math.floor(Math.random() * flags.length)].code;
 
 //urlflagchange
-var originalFlag = 'flags/' + flags[Math.floor(Math.random() * flags.length)].code.toLowerCase() + '.svg'
-var svgFlag = document.querySelectorAll('.flag img src');
-
+var originalFlag = 'flags/' + flags[Math.floor(Math.random() * flags.length)].code.toLowerCase() + '.svg';
+//var svgFlag =
+var flag = document.querySelectorAll('.flag  img');
 
 
 //test
 
+// random colors
+// liste conditions pour couleurs/puis titre = 1 drapeau/puis perte de vie + augmentation temps
 
-//randomCards and
+//function good flag() {}
+
+//randomCards
 function randomCards() {
-
+  for (var i = 0; i < flag.length; i++) {
+    flag[i].src = 'flags/' + flags[Math.floor(Math.random() * flags.length)].code.toLowerCase() + '.svg';
+  }
 }
 
 // Start game
@@ -37,7 +51,7 @@ startButton.addEventListener('click', function() {
   start.classList.remove('is-open');
   game.classList.add('is-open');
   title.textContent = randomTitle;
-
+  randomCards();
 });
 
 //Restart game
